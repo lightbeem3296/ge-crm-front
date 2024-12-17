26344be375fc14d4566c758ce1bbf067ea8d01c8
alpha <alpha5611331@gmail.com>
2024-12-17 12:35:15 -0500

    fixed mobile sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -92,31 +92,44 @@ export default function Sidebar({ mobileFiltersOpen, setMobileFiltersOpen }: Sid
         <div className="fixed inset-0 z-40 flex">
           <DialogPanel
             transition
-            className="relative ml-auto flex size-full max-w-xs transform flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl transition duration-300 ease-in-out data-[closed]:translate-x-full"
+            className="relative ml-auto flex size-full max-w-xs transform flex-col overflow-y-auto bg-neutral-900 py-4 pb-12 shadow-xl transition duration-300 ease-in-out data-[closed]:translate-x-full"
           >
-            <div className="flex items-center justify-between px-4">
-              <h2 className="text-lg font-medium text-gray-900">Filters</h2>
+            <div className="flex items-center justify-between px-4 border-b border-gray-600">
+              <div className='py-4'>
+                <p className='text-md text-gray-200 font-medium uppercase'>Admin Panel</p>
+                <p className='text-sm text-gray-400'>Employee & Payroll</p>
+              </div>
               <button
                 type="button"
                 onClick={() => setMobileFiltersOpen(false)}
-                className="-mr-2 flex size-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
+                className="-mr-2 flex size-10 items-center justify-center rounded-md p-2 text-gray-400"
               >
                 <span className="sr-only">Close menu</span>
                 <XMarkIcon aria-hidden="true" className="size-6" />
               </button>
             </div>
 
-            <form className="mt-4 border-t border-gray-200">
-              <ul role="list" className="px-2 py-3 font-medium text-gray-900">
-                {sideBarMenuItems.map((menuItem) => (
-                  <li key={menuItem.name}>
-                    <Link href={menuItem.href} className="block px-2 py-3">
+            <ul className="text-sm mt-4 px-4">
+              {sideBarMenuItems.map((menuItem) => (
+                menuItem.type === SidebarMenuItemType.MenuItem ? (
+                  <Link
+                    key={menuItem.name}
+                    href={menuItem.href}
+                  >
+                    <li className={`px-4 py-2 rounded-md border border-transparent hover:text-blue-600 hover:border-gray-800 ${pathname == menuItem.href
+                      ? "font-semibold text-blue-600 bg-gray-900 border-gray-800"
+                      : "font-normal text-gray-300"
+                      }`}>
                       {menuItem.name}
-                    </Link>
+                    </li>
+                  </Link>
+                ) : menuItem.type === SidebarMenuItemType.Title ? (
+                  <li key={menuItem.name} className="text-gray-500 uppercase font-medium py-2">
+                    {menuItem.name}
                   </li>
-                ))}
-              </ul>
-            </form>
+                ) : null
+              ))}
+            </ul>
           </DialogPanel>
         </div>
       </Dialog>
