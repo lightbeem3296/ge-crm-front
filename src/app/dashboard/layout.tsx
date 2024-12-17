fd4285913ebe663f4074e1af80004d96fa9297b9
alpha <alpha5611331@gmail.com>
2024-12-17 09:55:10 -0500

    splitted into sidebar, header and footer

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,24 @@
-import Footer from "@/components/layout/Footer";
-import Header from "@/components/layout/Header";
-import Sidebar from "@/components/layout/Sidebar";
+'use client'
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-    return (
-      <div>
-        <Header/>
-        <Sidebar/>
-        <main className="p-4">
+import { useState } from 'react'
+import Header from '@/components/layout/Header'
+import Sidebar from '@/components/layout/Sidebar'
+import Footer from '@/components/layout/Footer'
+
+
+export default function Example({ children }: { children: React.ReactNode }) {
+  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
+
+  return (
+    <div className="bg-white flex min-h-screen">
+      <Sidebar mobileFiltersOpen={mobileFiltersOpen} setMobileFiltersOpen={setMobileFiltersOpen} />
+      <main className="p-4 w-full">
+        <Header setMobileFiltersOpen={setMobileFiltersOpen} />
+        <div className='p-4'>
           {children}
-        </main>
-        <Footer/>
-      </div>
-    );
-  }
-  
+        </div>
+        <Footer />
+      </main>
+    </div>
+  )
+}
