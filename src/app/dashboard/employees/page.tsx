718b5f19b099fe80918a110e00c3089d517cab7c
alpha <alpha5611331@gmail.com>
2024-12-18 07:25:51 -0500

    added column filter

--- src/app/dashboard/employees/page.tsx
+++ src/app/dashboard/employees/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import React, { useRef, useState } from "react";
-import type { ColDef, GridApi, GridReadyEvent, IGetRowsParams, ValueGetterParams } from "ag-grid-community";
+import React, { useCallback, useRef, useState } from "react";
+import type { ColDef, ColGroupDef, GridApi, GridReadyEvent, IGetRowsParams, ValueGetterParams } from "ag-grid-community";
 import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
 import { AgGridReact } from "ag-grid-react";
 import axios from "axios";
@@ -11,7 +11,7 @@ import { listRoles as listAllRoles } from "@/services/roleService";
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 
-interface IColData {
+interface IRowData {
   _id: string,
   username: string;
   m_nr: number;
@@ -54,9 +54,9 @@ interface ServerResponse {
 }
 
 export default function EmployeesPage() {
-  const gridApiRef = useRef<GridApi | null>(null);
+  const [rowData, setRowData] = useState<IRowData[]>();
 
-  const [colDefs, setColDefs] = useState<ColDef<IColData>[]>([
+  const [colDefs, setColDefs] = useState<(ColDef<IRowData> | ColGroupDef)[]>([
     {
       headerName: "Name",
       field: "username",
@@ -78,12 +78,20 @@ export default function EmployeesPage() {
       width: 160,
     },
     {
-      headerName: "Employment Start Date",
-      field: "employment_start_date",
-    },
-    {
-      headerName: "Employment End Date",
-      field: "employment_end_date",
+      headerName: "Employment Date",
+      children: [
+        {
+          headerName: "Start Date",
+          field: "employment_start_date",
+          width: 200,
+          cellDataType: "dateString",
+        },
+        {
+          headerName: "End Date",
+          field: "employment_end_date",
+          width: 200,
+        },
+      ],
     },
     {
       headerName: "Salary Type",
@@ -124,82 +132,67 @@ export default function EmployeesPage() {
   ]);
 
   const defaultColDef: ColDef = {
-    // flex: 1,
+    minWidth: 100,
+    filter: true,
+    floatingFilter: true,
+    editable: true,
   };
 
-  const setDataSource = () => {
-    const dataSource = {
-      getRows: async (params: IGetRowsParams) => {
-        const { startRow, endRow } = params;
 
-        console.log('Requesting rows:', startRow, 'to', endRow);
+  const fetchRowData = async () => {
+    try {
+      const all_tags = await listAllTags();
+      const all_roles = await listAllRoles();
 
-        try {
-          const all_tags = await listAllTags();
-          const all_roles = await listAllRoles();
-
-          const response = await axios.get<ServerResponse>(
-            'http://localhost:8000/api/employee',
-            {
-              params: {
-                skip: startRow,
-                limit: endRow - startRow,
-              },
-            }
-          );
-          const items: IColData[] = [];
-          response.data.items.forEach(element => {
-            let tags: string = "";
-            element.tags.forEach((tag_id, i) => {
-              if (i === 0) {
-                tags = all_tags[tag_id].tag_name;
-              } else {
-                tags += ", " + all_tags[tag_id].tag_name;
-              }
-            });
-            items.push({
-              _id: element._id,
-              username: element.username,
-              m_nr: element.m_nr,
-              role: all_roles[element.role].role_name,
-              department: element.department,
-              employment_start_date: element.employment_start_date,
-              employment_end_date: element.employment_end_date,
-              salary_type: element.salary_type,
-              hourly_rate: element.hourly_rate,
-              salary: element.salary,
-              hours_worked: element.hours_worked,
-              bonus: element.bonus,
-              deduction: element.deduction,
-              tags: tags,
-            });
-          });
-          params.successCallback(items, response.data.total);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-          params.failCallback();
-        }
-      },
-    };
-
-    gridApiRef.current?.setGridOption("datasource", dataSource);
-  };
-
-  const onGridReady = (params: GridReadyEvent) => {
-    gridApiRef.current = params.api;
-    setDataSource();
+      const response = await axios.get<ServerResponse>(
+        'http://localhost:8000/api/employee',
+      );
+      const items: IRowData[] = [];
+      response.data.items.forEach(element => {
+        let tags: string = "";
+        element.tags.forEach((tag_id, i) => {
+          if (i === 0) {
+            tags = all_tags[tag_id].tag_name;
+          } else {
+            tags += ", " + all_tags[tag_id].tag_name;
+          }
+        });
+        items.push({
+          _id: element._id,
+          username: element.username,
+          m_nr: element.m_nr,
+          role: all_roles[element.role].role_name,
+          department: element.department,
+          employment_start_date: element.employment_start_date.split("T")[0],
+          employment_end_date: element.employment_end_date.split("T")[0],
+          salary_type: element.salary_type,
+          hourly_rate: element.hourly_rate,
+          salary: element.salary,
+          hours_worked: element.hours_worked,
+          bonus: element.bonus,
+          deduction: element.deduction,
+          tags: tags,
+        });
+      });
+      setRowData(items);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   }
+
+  const onGridReady = useCallback(async (params: GridReadyEvent) => {
+    await fetchRowData();
+  }, []);
+
 
   return (
     <div>
       <div className="text-lg font-medium px-2 py-4">Employees</div>
       <div className="h-[calc(100vh-10.6rem)] min-h-[450px] min-w-[800px]">
         <AgGridReact
-          rowModelType="infinite" // server-side pagination
           columnDefs={colDefs}
-          cacheBlockSize={10} // Page size (number of rows per request)
+          rowData={rowData}
           defaultColDef={defaultColDef}
-          maxBlocksInCache={0} // Cache the last 5 pages
           onGridReady={onGridReady} // Callback for grid ready
           pagination={true}
           paginationPageSize={10}
