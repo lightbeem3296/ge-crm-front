4ba1d0087655d0dc72cff21016c64f2c9df3ec88
alpha <alpha5611331@gmail.com>
2024-12-24 12:11:20 -0500

    done navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -12,20 +12,19 @@ interface HeaderProps {
 
 export default function Navbar({ setMobileFiltersOpen }: HeaderProps) {
   return (
-    <div className="flex items-center justify-between border-b border-gray-200 px-4 py-2 bg-gray-100/80 sticky top-0 backdrop-blur-md z-10">
+    <div className="flex items-center justify-between border-b border-base-300 px-4 py-2 bg-base-100/80 sticky top-0 backdrop-blur-md z-10">
       <div className='flex items-center gap-x-2'>
         <button
-          type="button"
           onClick={() => setMobileFiltersOpen(true)}
-          className="p-2 text-gray-400 hover:text-gray-500 lg:hidden"
+          className="btn btn-sm btn-ghost lg:hidden"
         >
           <Bars3Icon aria-hidden="true" className="size-5" />
         </button>
-        <h1 className="text-md font-bold tracking-tight text-gray-600">Navigation Bar</h1>
+        <h1 className="text-md font-bold tracking-tight text-base-content/70">Navigation Bar</h1>
       </div>
-      <div className="flex items-center">
+      <div className="flex gap-1 items-center">
         <ThemeController />
-        <button type="button" className="ml-2 p-2 text-gray-400 hover:text-gray-500">
+        <button className="btn btn-sm btn-ghost">
           <Squares2X2Icon aria-hidden="true" className="size-5" />
         </button>
       </div>
