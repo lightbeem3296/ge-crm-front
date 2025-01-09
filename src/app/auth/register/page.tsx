b89b1164ae9f55ecb5fca0fcccf5497ea45ae612
alpha <alpha5611331@gmail.com>
2025-01-09 07:41:52 -0500

    done register

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,209 @@
+"use client";
+
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import Link from "next/link";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { customAlert, CustomAlertType } from "@/components/ui/alert";
+
+interface FormData {
+  username: string;
+  password: string;
+  passwordConfirm: string;
+}
+
+const schema = yup.object().shape({
+  username: yup
+    .string()
+    .required("Username is required")
+    .min(3, "Username must be at least 3 characters long"),
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(6, "Password must be at least 6 characters long"),
+  passwordConfirm: yup
+    .string()
+    .required("Please confirm your password")
+    .oneOf([yup.ref("password")], "Passwords do not match"),
+});
+
+export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showPasswordConfirm, setShowPasswordConfirm] = useState<boolean>(false);
+  const router = useRouter();
+
+  const onSubmit = async (data: FormData) => {
+    console.log("Form Data Submitted:", data);
+    try {
+      const response = await axios.post("/api/auth/register", data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      customAlert({
+        type: CustomAlertType.SUCCESS,
+        title: "Registration success",
+        message: "User registered successfully. Login with registered user.",
+      });
+      router.push("/auth/login");
+    } catch (error: any) {
+      customAlert({
+        type: CustomAlertType.ERROR,
+        title: "Registration failed",
+        message: error.response?.data.detail || error.message,
+      });
+    }
+  };
+
+  // UI Handlers
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  }
+
+  const handleClickShowPasswordConfirm = () => {
+    setShowPasswordConfirm(!showPasswordConfirm);
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-4 max-w-96 mx-auto border px-8 py-16 rounded-md mt-20"
+    >
+      <div className="flex justify-center mb-8">
+        <span className="font-sans font-medium text-xl">
+          Register New User
+        </span>
+      </div>
+
+      {/* Username field */}
+      <div className="flex flex-col">
+        <label className="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70">
+            <path
+              d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+          </svg>
+          <input
+            type="text"
+            id="username"
+            className="grow"
+            placeholder="Username"
+            {...register("username")}
+          />
+
+        </label>
+        {errors.username && (
+          <p className="text-red-500 text-sm mt-1">{errors.username.message}</p>
+        )}
+      </div>
+
+      {/* Password field */}
+      <div className="flex flex-col">
+        <label className="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70">
+            <path
+              fillRule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clipRule="evenodd" />
+          </svg>
+          <input
+            type={showPassword ? "text" : "password"}
+            id="password"
+            className="grow"
+            placeholder="Password"
+            {...register("password")}
+          />
+          <button
+            type="button"
+            className={!watch("password") ? "hidden" : ""}
+            onClick={() => handleClickShowPassword()}
+          >
+            {showPassword
+              ? <FontAwesomeIcon icon={faEyeSlash} width={12} />
+              : <FontAwesomeIcon icon={faEye} width={12} />}
+          </button>
+
+        </label>
+        {errors.password && (
+          <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>
+        )}
+      </div>
+
+      {/* Confirm password field */}
+      <div className="flex flex-col">
+        <label className="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70">
+            <path
+              fillRule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clipRule="evenodd" />
+          </svg>
+          <input
+            type={showPasswordConfirm ? "text" : "password"}
+            id="passwordConfirm"
+            className="grow"
+            placeholder="Password Confirm"
+            {...register("passwordConfirm")}
+          />
+          <button
+            type="button"
+            className={!watch("passwordConfirm") ? "hidden" : ""}
+            onClick={() => handleClickShowPasswordConfirm()}
+          >
+            {showPasswordConfirm
+              ? <FontAwesomeIcon icon={faEyeSlash} width={12} />
+              : <FontAwesomeIcon icon={faEye} width={12} />}
+          </button>
+        </label>
+        {errors.passwordConfirm && (
+          <p className="text-red-500 text-sm mt-1">
+            {errors.passwordConfirm.message}
+          </p>
+        )}
+      </div>
+
+      {/* Submit button */}
+      <button
+        type="submit"
+        className="btn btn-info"
+      >
+        Register
+      </button>
+      <div className="flex gap-2">
+        <span>
+          Already have account?
+        </span>
+        <Link
+          href="/auth/login"
+          className="link"
+        >
+          Go to login
+        </Link>
+      </div>
+    </form>
+  )
+}
