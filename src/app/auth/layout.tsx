48e5a957bda1496337487445d5339cab00a2f6fb
alpha <alpha5611331@gmail.com>
2025-01-23 13:17:29 -0500

    fixed auth page theme

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,9 +1,26 @@
 'use client'
 
+import { useEffect, useState } from "react";
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    if (localStorage) {
+      const theme = localStorage.getItem("theme");
+      document.documentElement.setAttribute("data-theme", theme || "light");
+      setLoading(false);
+    }
+  }, []);
+
   return (
-    <div className="flex min-h-screen bg-primary/5">
-      {children}
-    </div>
+    <>
+      {
+        loading
+          ? null
+          : <div className="flex min-h-screen bg-primary/5">
+            {children}
+          </div>
+      }
+    </>
   )
 }
